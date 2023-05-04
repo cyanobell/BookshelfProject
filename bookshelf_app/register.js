3e656660b68230c583da8060173ba79427d59929
cyanobell <cyanobell@gmail.com>
2023-05-04 20:07:38 +0900

    helmetを使う、MySQLStoreの追加 SQL文の見直しはどを行った。

--- bookshelf_app/register.js
+++ bookshelf_app/register.js
@@ -31,6 +31,10 @@ class Register extends React.Component {
           this.setState({
             login_state_text: 'その名前はすでに登録されています'
           });
+        } else if (json.text === 'The name or pass is empty.') {
+          this.setState({
+            login_state_text: 'ユーザー名かパスワードが空です'
+          });
         } else if (json.text === 'success') {
           location.href = '/home';
         }
--- routes/sharedBooks.js
+++ routes/sharedBooks.js
@@ -3,9 +3,19 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.get('/:shared_id', (req, res) => {
+router.get('/:shared_id', async (req, res) => {
+    const connection = req.app.locals.connection;
     const crientDirectry = req.app.locals.crientDirectry;
-    res.sendFile(path.join(__dirname, '..', crientDirectry, 'readOnly.html'));
+    try {
+        const users = await connection.queryAsync('SELECT * FROM users WHERE id = ?', [req.params.shared_id]);
+        if (users.length !== 1) {
+            res.redirect('/');
+            return;
+        }
+        res.sendFile(path.join(__dirname, '..', crientDirectry, 'readOnly.html'));
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 module.exports = router;
