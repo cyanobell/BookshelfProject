cd2243eacbbb3780d0403973063ac11bedcec109
cyanobell <cyanobell@gmail.com>
2023-05-07 09:29:04 +0900

    不要なコンストラクタの削除#35

--- BookShelfDrogonServer/controllers/HomeCtrl.h
+++ BookShelfDrogonServer/controllers/HomeCtrl.h
@@ -5,8 +5,6 @@ class HomeCtrl : public drogon::HttpController<HomeCtrl>
 {
   constexpr static const char *html_filename = "../../bookshelf_app/home.html";
 public:
-  HomeCtrl() : drogon::HttpController<HomeCtrl>()
-  {}
 
   METHOD_LIST_BEGIN
   ADD_METHOD_TO(HomeCtrl::get,"/home", drogon::Get);
