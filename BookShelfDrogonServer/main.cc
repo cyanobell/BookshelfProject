d5d0ddc0c1110e5b0e011fd9a1dbc8faca6cfaf5
cyanobell <cyanobell@gmail.com>
2023-05-12 11:14:30 +0900

    再起動設定や、logファイルの出力など、サーバーの変更

--- BookShelfDrogonServer/main.cc
+++ BookShelfDrogonServer/main.cc
@@ -4,10 +4,12 @@
 int main()
 {
     // Set HTTP listener address and port
+    LOG_INFO << "server awaken";
     drogon::app().loadConfigFile("../config.json");
     constexpr static const char *e404_filename = "../../bookshelf_app/404.html";
     auto res = drogon::HttpResponse::newFileResponse(e404_filename);
     drogon::app().setCustom404Page(res);
     drogon::app().run();
+    LOG_INFO << "server closed";
     return 0;
 }
